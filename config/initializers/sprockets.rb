a9f47b0a564f47086096a90ec7917db15a4d729b
Marten Klitzke <me@mortik.xyz>
2016-06-30 01:07:46 +0200

    Use relative paths for sprockets processor

--- config/initializers/sprockets.rb
+++ config/initializers/sprockets.rb
@@ -1,7 +1,8 @@
+
 class Sprockets::DirectiveProcessor
   def process_depend_on_locales_directive
-    Dir.glob(Rails.root.join("config","locales", "**.yml").to_s).each do |path|
-      context.depend_on(path)
+    Dir.glob(Rails.root.join("config", "locales", "**.yml").to_s).each do |path|
+      resolve(File.basename(path))
     end
   end
 end
